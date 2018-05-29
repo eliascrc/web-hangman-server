babc273f46834dca53872b2b18c3b35cca2771ba
danielmdob <danielmdob@users.noreply.github.com>
2018-05-28 18:53:24 -0600

    Started progress on saving highscores

--- hangman.php
+++ hangman.php
@@ -1,39 +1,73 @@
 <?php
 class Hangman {
+	private $playerName;
+	private $originalSolution;
 	private $solution;
 	private $progress;
 	private $maskedProgress;
 	private $tries;
+	private $failedLetters;
+	private $startTime;
 	
-	function __construct() {
-		$this->solution = "hangman";
+	function __construct($solution, $playerName) {
+		$this->originalSolution = $solution;
+		$this->solution = $solution;
 		$this->progress = preg_replace("/(.)/", "*", $this->solution);
-		$this->tries = 1000;
+		$this->tries = 7;
+		$this->failedLetters = array();
+		$this->playerName = $playerName;
+		$this->startTime = time();
+	}
+	
+	public function play($guessLetter) {
+		$triesAndProgress = array();
+		if ($this->tries > 1) {
+			$triesAndProgress[1] = $this->makeGuess($guessLetter);
+			$triesAndProgress[0] = $this->tries;
+		}
+		
+		return $triesAndProgress;
 	}
 	
 	public function makeGuess($guessLetter) {
-		$position = strpos($this->solution, $guessLetter);
+		$position = strpos($this->originalSolution, $guessLetter);
 		
-		if ($this->tries > 1) {
-			if ($position === false) {
-				$this->tries--;
-			}
-		
-			while($position !== false) {
-				$this->progress[$position] = $this->solution[$position];
-				$this->solution[$position] = "*";
-				$position = strpos($this->solution, $guessLetter);
-			}
-			
-			return $this->progress;
-		} else {
-			return false;
+		if ($position === false && !in_array($guessLetter, $this->failedLetters)) {
+			array_push($this->failedLetters, $guessLetter);
+			$this->tries--;
+		}
+	
+		while($position !== false) {
+			$this->progress[$position] = $this->originalSolution[$position];
+			$this->solution[$position] = "*";
+			$position = strpos($this->solution, $guessLetter);
 		}
 		
+		if(strpos($this->originalSolution, "*") === false) {
+			$this->saveScore(time() - $startTime);
+		}
+		
+		return $this->progress;
 	}
 	
+	public function serialice($duration) {
+		return $this->playerName.':'.$this->originalSolution.':'.$duration;
+	}
+	
+	public function saveScore($duration) {
+		$file = fopen('highscores.txt', 'a+');
+		if (fwrite($file, $this->serialice($duration)."\n"))
+		{
+			fclose($file);
+			return true;
+		}
+		else
+		{
+			fclose($file);
+			return false;
+		}
+	}
 }
 
-$hangman = new Hangman();
-echo $hangman->makeGuess("a");
-echo $hangman->makeGuess("h");
+$hangman = new Hangman("solution", "name");
+echo $hangman->serialice(time());
